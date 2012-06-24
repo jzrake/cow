1d893c7b74f127582284ebbb7a53a334a9d1275b
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-06-24 12:50:21 -0400

    Have cartesian communicator setup working.

--- main.c
+++ main.c
@@ -8,57 +8,70 @@
 #include <mpi.h>
 #endif
 
-struct cow_dfield; // forward declarations (for opaque data structure)
-struct cow_domain;
-
-struct cow_dfield *cow_domain_addfield(struct cow_domain *d, const char *name);
-struct cow_dfield *cow_domain_getfield(struct cow_domain *d, const char *name);
-struct cow_dfield *cow_dfield_new();
-struct cow_domain *cow_domain_new();
-void cow_dfield_del(struct cow_dfield *f);
-void cow_dfield_addmember(struct cow_dfield *f, const char *name);
-void cow_dfield_setname(struct cow_dfield *f, const char *name);
-const char *cow_dfield_getname(struct cow_dfield *f);
-
-void cow_domain_setsize(struct cow_domain *d, int dim, int size);
-void cow_domain_setndim(struct cow_domain *d, int ndim);
-void cow_domain_setguard(struct cow_domain *d, int guard);
-void cow_domain_commit(struct cow_domain *d);
-struct cow_dfield *cow_domain_iteratefields(struct cow_domain *d);
-struct cow_dfield *cow_domain_nextfield(struct cow_domain *d);
-
-const char *cow_dfield_iteratemembers(struct cow_dfield *f);
-const char *cow_dfield_nextmember(struct cow_dfield *f);
-
-void cow_dfield_commit(struct cow_dfield *f);
-int cow_domain_getnumlocalzones(struct cow_domain *d);
 
 // -----------------------------------------------------------------------------
 //
-// struct cow_domain interface functions
+// These prototypes constitute the C.O.W. interface
+//
+// -----------------------------------------------------------------------------
+struct cow_dfield; // forward declarations (for opaque data structure)
+struct cow_domain;
+typedef struct cow_dfield cow_dfield;
+typedef struct cow_domain cow_domain;
+
+cow_domain *cow_domain_new();
+void cow_domain_commit(cow_domain *d);
+void cow_domain_del(cow_domain *d);
+cow_dfield *cow_domain_addfield(cow_domain *d, const char *name);
+cow_dfield *cow_domain_getfield(cow_domain *d, const char *name);
+void cow_domain_setsize(cow_domain *d, int dim, int size);
+void cow_domain_setndim(cow_domain *d, int ndim);
+void cow_domain_setguard(cow_domain *d, int guard);
+void cow_domain_setprocsizes(cow_domain *d, int dim, int size);
+cow_dfield *cow_domain_iteratefields(cow_domain *d);
+cow_dfield *cow_domain_nextfield(cow_domain *d);
+
+cow_dfield *cow_dfield_new();
+void cow_dfield_commit(cow_dfield *f);
+void cow_dfield_del(cow_dfield *f);
+void cow_dfield_addmember(cow_dfield *f, const char *name);
+void cow_dfield_setname(cow_dfield *f, const char *name);
+const char *cow_dfield_getname(cow_dfield *f);
+const char *cow_dfield_iteratemembers(cow_dfield *f);
+const char *cow_dfield_nextmember(cow_dfield *f);
+int cow_domain_getnumlocalzones(cow_domain *d);
+
+
+// -----------------------------------------------------------------------------
+//
+// cow_domain interface functions
 //
 // -----------------------------------------------------------------------------
 struct cow_domain
 {
-  double coord_lower[3]; // lower coordinates of physical domain
-  double coord_upper[3]; // upper " "
-  int A_nint[3];
-  int L_ntot[3];
-  int L_strt[3];
-  int G_ntot[3];
-  int G_strt[3];
-  int n_dims;
-  int n_ghst;
-  int n_fields;
-  int field_iter;
-  int committed;
-  struct cow_dfield **fields; // array of pointers to data fields
+  double glb_lower[3]; // lower coordinates of global physical domain
+  double glb_upper[3]; // upper " "
+  double loc_lower[3]; // lower coordinates of local physical domain
+  double loc_upper[3]; // upper " "
+  int L_nint[3]; // interior zones on the local subgrid
+  int L_ntot[3]; // total " ", including guard zones
+  int L_strt[3]; // starting index of interior zones on local subgrid
+  int G_ntot[3]; // global domain size
+  int G_strt[3]; // starting index into global domain
+  int n_dims; // number of dimensions: 1, 2, 3
+  int n_ghst; // number of guard zones: >= 0
+  int n_fields; // number of data fields (dynamically adjustable)
+  int field_iter; // index into data fields array used for iterating over them
+  int committed; // true after cow_domain_commit called, locks out most changes
+  cow_dfield **fields; // array of pointers to data fields
 
 #if (COW_MPI)
   int comm_rank; // rank with respect to MPI_COMM_WORLD communicator
   int comm_size; // size " "
   int cart_rank; // rank with respect to the cartesian communicator
   int cart_size; // size " "
+  int proc_sizes[3]; // number of subgrids along each dimension
+  int proc_index[3]; // coordinates of local subgrid in cartesian communicator
   int num_neighbors; // 3, 9, or 27 depending on the domain dimensionality
   int *neighbors; // cartesian ranks of the neighboring processors
   int *send_tags; // tag used to on send calls with respective neighbor
@@ -71,11 +84,13 @@ struct cow_domain
 
 struct cow_domain *cow_domain_new()
 {
-  struct cow_domain *d = (struct cow_domain*) malloc(sizeof(struct cow_domain));
-  struct cow_domain dom = {
-    .coord_lower = { 0.0, 0.0, 0.0 },
-    .coord_upper = { 1.0, 1.0, 1.0 },
-    .A_nint = { 1, 1, 1 },
+  cow_domain *d = (cow_domain*) malloc(sizeof(cow_domain));
+  cow_domain dom = {
+    .glb_lower = { 0.0, 0.0, 0.0 },
+    .glb_upper = { 1.0, 1.0, 1.0 },
+    .loc_lower = { 0.0, 0.0, 0.0 },
+    .loc_upper = { 1.0, 1.0, 1.0 },
+    .L_nint = { 1, 1, 1 },
     .L_ntot = { 1, 1, 1 },
     .L_strt = { 1, 1, 1 },
     .G_ntot = { 1, 1, 1 },
@@ -85,23 +100,30 @@ struct cow_domain *cow_domain_new()
     .n_fields = 0,
     .field_iter = 0,
     .committed = 0,
-    .fields = NULL
+    .fields = NULL,
+#if (COW_MPI)
+    .proc_sizes = { 0, 0, 0 },
+    .proc_index = { 0, 0, 0 },
+#endif
   } ;
   *d = dom;
   return d;
 }
-void cow_domain_del(struct cow_domain *d)
+void cow_domain_del(cow_domain *d)
 {
+#if (COW_MPI)
+  MPI_Comm_free(&d->mpi_cart);
+#endif
   for (int n=0; n<d->n_fields; ++n) cow_dfield_del(d->fields[n]);
   free(d->fields);
   free(d);
 }
-struct cow_dfield *cow_domain_addfield(struct cow_domain *d, const char *name)
+cow_dfield *cow_domain_addfield(cow_domain *d, const char *name)
 {
   d->n_fields++;
-  d->fields = (struct cow_dfield**) realloc(d->fields,
-					    d->n_fields*sizeof(struct cow_dfield*));
-  struct cow_dfield *f = cow_dfield_new(d);
+  d->fields = (cow_dfield**) realloc(d->fields,
+                                     d->n_fields*sizeof(cow_dfield*));
+  cow_dfield *f = cow_dfield_new(d);
   cow_dfield_setname(f, name);
   if (d->committed) {
     // -------------------------------------------------------------------------
@@ -113,7 +135,7 @@ struct cow_dfield *cow_domain_addfield(struct cow_domain *d, const char *name)
   }
   return d->fields[d->n_fields-1] = f;
 }
-struct cow_dfield *cow_domain_getfield(struct cow_domain *d, const char *name)
+cow_dfield *cow_domain_getfield(cow_domain *d, const char *name)
 {
   for (int n=0; n<d->n_fields; ++n) {
     if (strcmp(cow_dfield_getname(d->fields[n]), name) == 0) {
@@ -122,55 +144,104 @@ struct cow_dfield *cow_domain_getfield(struct cow_domain *d, const char *name)
   }
   return NULL;
 }
-struct cow_dfield *cow_domain_iteratefields(struct cow_domain *d)
+cow_dfield *cow_domain_iteratefields(cow_domain *d)
 {
   d->field_iter = 0;
   return cow_domain_nextfield(d);
 }
-struct cow_dfield *cow_domain_nextfield(struct cow_domain *d)
+cow_dfield *cow_domain_nextfield(cow_domain *d)
 {
   return d->field_iter++ < d->n_fields ? d->fields[d->field_iter-1] : NULL;
 }
-void cow_domain_setsize(struct cow_domain *d, int dim, int size)
+void cow_domain_setsize(cow_domain *d, int dim, int size)
 {
-  if (dim > 3) return;
+  if (dim > 3 || d->committed) return;
   d->G_ntot[dim] = size;
 }
-void cow_domain_setndim(struct cow_domain *d, int ndim)
+void cow_domain_setndim(cow_domain *d, int ndim)
 {
-  if (ndim > 3) return;
+  if (ndim > 3 || d->committed) return;
   d->n_dims = ndim;
 }
-void cow_domain_setguard(struct cow_domain *d, int guard)
+void cow_domain_setguard(cow_domain *d, int guard)
 {
+  if (guard < 0 || d->committed) return;
   d->n_ghst = guard;
 }
-void cow_domain_commit(struct cow_domain *d)
+void cow_domain_setprocsizes(cow_domain *d, int dim, int size)
+{
+#if (COW_MPI)
+  if (dim > 3 || d->committed) return;
+  d->proc_sizes[dim] = size;
+#endif
+}
+void cow_domain_commit(cow_domain *d)
 {
   if (d->committed) return;
 
 #if (COW_MPI)
+  int w[3] = { 1, 1, 1 }; // `wrap`, periodic in all directions
+  int r = 1; // `reorder` allow MPI to choose a cart_rank != comm_rank
+
   MPI_Comm_rank(MPI_COMM_WORLD, &d->comm_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &d->comm_size);
-#endif
+  MPI_Dims_create(d->comm_size, d->n_dims, d->proc_sizes);
+  MPI_Cart_create(MPI_COMM_WORLD, d->n_dims, d->proc_sizes, w, r, &d->mpi_cart);
+  MPI_Comm_rank(d->mpi_cart, &d->cart_rank);
+  MPI_Cart_coords(d->mpi_cart, d->cart_rank, d->n_dims, d->proc_index);
 
-  d->L_ntot[0] = d->G_ntot[0] + 2 * d->n_ghst;
-  d->L_ntot[1] = d->G_ntot[1] + 2 * d->n_ghst;
-  d->L_ntot[2] = d->G_ntot[2] + 2 * d->n_ghst;
+  for (int i=0; i<d->n_dims; ++i) {
+    // -------------------------------------------------------------------------
+    // The number of subgrid zones for dimension i needs to be non-uniform if
+    // proc_sizes[i] does not divide the G_ntot[i]. For each dimension, we add a
+    // zone to the first R subgrids , where R is given below:
+    // -------------------------------------------------------------------------
+    const int R = d->G_ntot[i] % d->proc_sizes[i];
+    const int normal_size = d->G_ntot[i] / d->proc_sizes[i];
+    const int augmnt_size = normal_size + 1;
+    const int thisdm_size = (d->proc_index[i]<R) ? augmnt_size : normal_size;
+    const double dx = (d->glb_upper[i] - d->glb_lower[i]) / d->G_ntot[i];
+
+    d->L_nint[i] = thisdm_size;
+    d->G_strt[i] = 0;
+    for (int j=0; j<d->proc_index[i]; ++j) {
+      d->G_strt[i] += (j<R) ? augmnt_size : normal_size;
+    }
+    d->loc_lower[i] = d->glb_lower[i] + dx *  d->G_strt[i];
+    d->loc_upper[i] = d->glb_upper[i] + dx * (d->G_strt[i] + thisdm_size);
+    d->L_ntot[i] = d->L_nint[i] + 2 * d->n_ghst;
+    d->L_strt[i] = d->n_ghst;
+  }
+
+  printf("[cow] subgrid layout is (%d %d %d)\n",
+         d->proc_sizes[0], d->proc_sizes[1], d->proc_sizes[2]);
+  //  printf("[cow] this domain has size (%d %d %d)\n",
+  //         d->L_nint[0], d->L_nint[1], d->L_nint[2]);
+#else
+  for (int i=0; i<d->n_dims; ++i) {
+    d->L_nint[i] = d->G_ntot[i];
+    d->L_ntot[i] = d->G_ntot[i] + 2 * d->n_ghst;
+    d->L_strt[i] = d->n_ghst;
+    d->G_strt[i] = 0;
+
+    d->loc_lower[i] = d->glb_lower[i];
+    d->loc_upper[i] = d->glb_upper[i];
+  }
+#endif
 
   for (int n=0; n<d->n_fields; ++n) {
     cow_dfield_commit(d->fields[n]);
   }
   d->committed = 1;
 }
-int cow_domain_getnumlocalzones(struct cow_domain *d)
+int cow_domain_getnumlocalzones(cow_domain *d)
 {
   return d->L_ntot[0] * d->L_ntot[1] * d->L_ntot[2];
 }
 
 // -----------------------------------------------------------------------------
 //
-// struct cow_dfield interface functions
+// cow_dfield interface functions
 //
 // -----------------------------------------------------------------------------
 struct cow_dfield
@@ -181,11 +252,11 @@ struct cow_dfield
   int n_members;
   void *data;
   int committed;
-  struct cow_domain *domain;
+  cow_domain *domain;
 } ;
-struct cow_dfield *cow_dfield_new(struct cow_domain *domain)
+cow_dfield *cow_dfield_new(cow_domain *domain)
 {
-  struct cow_dfield *f = (struct cow_dfield*) malloc(sizeof(struct cow_dfield));
+  cow_dfield *f = (cow_dfield*) malloc(sizeof(cow_dfield));
   f->name = NULL;
   f->members = NULL;
   f->n_members = 0;
@@ -195,7 +266,7 @@ struct cow_dfield *cow_dfield_new(struct cow_domain *domain)
   f->domain = domain;
   return f;
 }
-void cow_dfield_del(struct cow_dfield *f)
+void cow_dfield_del(cow_dfield *f)
 {
   for (int n=0; n<f->n_members; ++n) free(f->members[n]);
   free(f->members);
@@ -203,17 +274,17 @@ void cow_dfield_del(struct cow_dfield *f)
   free(f->data);
   free(f);
 }
-void cow_dfield_setname(struct cow_dfield *f, const char *name)
+void cow_dfield_setname(cow_dfield *f, const char *name)
 {
   if (f->committed) return;
   f->name = (char*) realloc(f->name, strlen(name)+1);
   strcpy(f->name, name);
 }
-const char *cow_dfield_getname(struct cow_dfield *f)
+const char *cow_dfield_getname(cow_dfield *f)
 {
   return f->name;
 }
-void cow_dfield_add_member(struct cow_dfield *f, const char *name)
+void cow_dfield_add_member(cow_dfield *f, const char *name)
 {
   if (f->committed) return;
   f->n_members++;
@@ -221,16 +292,16 @@ void cow_dfield_add_member(struct cow_dfield *f, const char *name)
   f->members[f->n_members-1] = (char*) malloc(strlen(name)+1);
   strcpy(f->members[f->n_members-1], name);
 }
-const char *cow_dfield_iteratemembers(struct cow_dfield *f)
+const char *cow_dfield_iteratemembers(cow_dfield *f)
 {
   f->member_iter = 0;
   return cow_dfield_nextmember(f);
 }
-const char *cow_dfield_nextmember(struct cow_dfield *f)
+const char *cow_dfield_nextmember(cow_dfield *f)
 {
   return f->member_iter++ < f->n_members ? f->members[f->member_iter-1] : NULL;
 }
-void cow_dfield_commit(struct cow_dfield *f)
+void cow_dfield_commit(cow_dfield *f)
 {
   if (f->committed) return;
   const int n_zones = cow_domain_getnumlocalzones(f->domain);
@@ -238,15 +309,21 @@ void cow_dfield_commit(struct cow_dfield *f)
   f->committed = 1;
 }
 
-int main()
+int main(int argc, char **argv)
 {
-  struct cow_domain *domain = cow_domain_new();
-  struct cow_dfield *prim = cow_domain_addfield(domain, "primitive");
-  struct cow_dfield *magf = cow_domain_addfield(domain, "magnetic");
-
 #if (COW_MPI)
-  printf("was compiled with COW_MPI\n");
+  {
+    int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank != 0) freopen("/dev/null", "w", stdout);
+    printf("was compiled with COW_MPI\n");
+  }
 #endif
+
+  cow_domain *domain = cow_domain_new();
+  cow_dfield *prim = cow_domain_addfield(domain, "primitive");
+  cow_dfield *magf = cow_domain_addfield(domain, "magnetic");
 
   cow_dfield_add_member(prim, "vx");
   cow_dfield_add_member(prim, "vy");
@@ -256,17 +333,25 @@ int main()
   cow_dfield_add_member(magf, "By");
   cow_dfield_add_member(magf, "Bz");
 
+  cow_domain_setndim(domain, 3);
+  cow_domain_setguard(domain, 2);
+  cow_domain_setsize(domain, 0, 12);
+  cow_domain_setsize(domain, 1, 13);
+  cow_domain_setsize(domain, 2, 14);
   cow_domain_commit(domain);
 
-  for (struct cow_dfield *d = cow_domain_iteratefields(domain);
+  for (cow_dfield *d = cow_domain_iteratefields(domain);
        d != NULL; d = cow_domain_nextfield(domain)) {
     printf("%s\n", cow_dfield_getname(d));
     for (const char *m = cow_dfield_iteratemembers(d);
-	 m != NULL; m = cow_dfield_nextmember(d)) {
+         m != NULL; m = cow_dfield_nextmember(d)) {
       printf("\t%s\n", m);
     }
   }
 
   cow_domain_del(domain);
+#if (COW_MPI)
+  MPI_Finalize();
+#endif
   return 0;
 }
