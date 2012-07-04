a63665fb3f7caf22405ac88d1c1e8bbcd14d6bc0
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-07-03 17:06:45 -0700

    Change to makehist for use on pleiades, might be temporary.

--- makehist.c
+++ makehist.c
@@ -40,14 +40,16 @@ int main(int argc, char **argv)
     printf("usage: $> makehist infile.h5 group member outfile.h5\n");
     goto done;
   }
-  char wholedset[1024];
-  snprintf(wholedset, 1024, "%s/%s", grou, memb);
+  char wholedset1[1024];
+  char wholedset2[1024];
+  snprintf(wholedset1, 1024, "%s/%s", grou, memb);
+  snprintf(wholedset2, 1024, "%s-hist", grou);
 
   int collective = GETENVINT("COW_HDF5_COLLECTIVE", 0);
   printf("COW_HDF5_COLLECTIVE: %d\n", collective);
 
   cow_domain *domain = cow_domain_new();
-  cow_domain_readsize(domain, finp, wholedset);
+  cow_domain_readsize(domain, finp, wholedset1);
   cow_domain_setguard(domain, 2);
   cow_domain_commit(domain);
   cow_domain_setchunk(domain, 1);
@@ -65,10 +67,10 @@ int main(int argc, char **argv)
   cow_histogram *hist = cow_histogram_new();
   cow_histogram_setlower(hist, 0, reduc[0]);
   cow_histogram_setupper(hist, 0, reduc[1]);
-  cow_histogram_setnbins(hist, 0, 100);
+  cow_histogram_setnbins(hist, 0, 500);
   cow_histogram_setbinmode(hist, COW_HIST_BINMODE_COUNTS);
   cow_histogram_commit(hist);
-  cow_histogram_setnickname(hist, memb);
+  cow_histogram_setnickname(hist, wholedset2);
   cow_histogram_populate(hist, f, take_elem0);
   cow_histogram_dumphdf5(hist, fout, "");
   cow_histogram_del(hist);
