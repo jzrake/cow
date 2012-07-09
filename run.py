2b6f00858d8da6fb2e015ab51492a37e065fe7d9
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-07-09 08:06:40 -0400

    Added dump method, @property semantics to value.

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ atexit.register(cow_exit)
 cow_init()
 
 
-class UnigridDatafield:
+class UnigridDatafield(object):
     def __init__(self, domain, members, name="dfield"):
         assert(type(name) is str)
         self._cdfield = cow_dfield_new(domain._cdomain, name)
@@ -23,14 +23,21 @@ class UnigridDatafield:
             cow_dfield_addmember(self._cdfield, m)
         cow_dfield_commit(self._cdfield)
         self._domain = domain
-        self.value = cow_dfield_getarray(self._cdfield)
 
     def __del__(self):
         print "deleting dfield"
         cow_dfield_del(self._cdfield)
 
+    @property
+    def value(self):
+        return cow_dfield_getarray(self._cdfield)
 
-class UnigridDomain:
+    def dump(self, fname):
+        assert(type(fname) is str)
+        cow_dfield_write(self._cdfield, fname)
+
+
+class UnigridDomain(object):
     def __init__(self, G_ntot, guard=0, x0=None, x1=None):
         self._nd = len(G_ntot)
         assert(self._nd <= 3)
@@ -50,6 +57,7 @@ def test():
     domain = UnigridDomain([10,10,10], guard=3)
     dfield = UnigridDatafield(domain, ["vx", "vy", "vz"])
     dfield.value[4:5,:,:,0] = 2.2
+    dfield.dump("pyout.h5")
     print dfield.value.max()
 
 
