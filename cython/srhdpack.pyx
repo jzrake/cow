574f163834e4c93f5da0d47f966cbb4a78de7f00
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-07-30 05:47:16 -0400

    Corrects for periodic BC's in srhdpack sampling
    There's a log entry describing the effect here:
    https://dl-web.dropbox.com/get/Work/srturb/log-07-30-2012.org?w=6218d4be

--- cython/srhdpack.pyx
+++ cython/srhdpack.pyx
@@ -10,8 +10,8 @@ cdef extern from "srhdpack.h":
                                        int seed)
 
 def relative_lorentz_pairs(DataField vel, nsamples, bins=36, nperbatch=10000, seed=True):
-    histpro = Histogram1d(0.0, 2.5, bins=bins, spacing="linear", commit=False)
-    histlab = Histogram1d(0.0, 2.5, bins=bins, spacing="linear", commit=False)
+    histpro = Histogram1d(0.0, 1.5, bins=bins, spacing="linear", commit=False)
+    histlab = Histogram1d(0.0, 1.5, bins=bins, spacing="linear", commit=False)
     srhdpack_relativelorentzpairs(vel._c, histpro._c, histlab._c,
                                   int(nsamples/nperbatch), nperbatch, seed)
     return histpro, histlab
